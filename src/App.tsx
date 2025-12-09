22adc53f4ab6e7d9455173fabe222b62e4fd7b70
David Flynn <david.flynn@vgw.co>
2025-12-09 15:05:21 +0800

    use header and footer, deleted top-nav (replaced by header)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
-import TopNav from './common/top-nav';
-import Home from './pages/home';
 import About from './pages/about';
+import Footer from './common/footer';
+import Header from './common/header';
+import Home from './pages/home';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <TopNav className="top-nav" />
-      </header>
+      <Header
+        links={[
+          { text: 'Home', url: '/' },
+          { text: 'About', url: '/about' },
+        ]}
+      />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
+      <Footer
+        copyright="flydav"
+        links={[
+          {
+            text: 'GitHub',
+            url: 'https://github.com/dflynn-vgw/react-scratch',
+          },
+        ]}
+      />
     </BrowserRouter>
   );
 }
