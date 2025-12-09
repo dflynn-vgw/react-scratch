7e0daa64403d1b06fed788099ee0d636de185a80
David Flynn <david.flynn@vgw.co>
2025-12-09 15:38:19 +0800

    use responsive grid layout for about page and set a minimum width to the body

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,62 +1,29 @@
+import './about.css';
+
 export default function about() {
   return (
     <section>
       <title>About</title>
       <h1>About</h1>
       <h2>Our Mission</h2>
-      /* Generate 3 paragraphs of lorem ipsum on 3 seperate columns */
-      <div className="columns">
-        <div className="column">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam.
-          </p>
-        </div>
-        <div className="column">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam.
-          </p>
-        </div>
-        <div className="column">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam.
-          </p>
-        </div>
-      </div>
-      <div className="columns">
-        <div className="column">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam.
-          </p>
-        </div>
-        <div className="column">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam.
-          </p>
-        </div>
-        <div className="column">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
-            velit sed velit aliquam, vel aliquet velit aliquam.
-          </p>
-        </div>
-      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit
+        sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit sed
+        velit aliquam, vel aliquet velit aliquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit
+        sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit sed
+        velit aliquam, vel aliquet velit aliquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit
+        sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit sed
+        velit aliquam, vel aliquet velit aliquam.
+      </p>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
         velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit
