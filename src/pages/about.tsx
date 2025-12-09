0951c6998facc747cdb354544d1e5b9d93e862c9
David Flynn <david.flynn@vgw.co>
2025-12-09 15:06:10 +0800

    updated about.tsx content, index.css now only has top level css

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,74 @@
 export default function about() {
   return (
-    <div>
+    <section>
       <title>About</title>
-      <p>This is the about page.</p>
-    </div>
+      <h1>About</h1>
+      <h2>Our Mission</h2>
+      /* Generate 3 paragraphs of lorem ipsum on 3 seperate columns */
+      <div className="columns">
+        <div className="column">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam.
+          </p>
+        </div>
+        <div className="column">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam.
+          </p>
+        </div>
+        <div className="column">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam.
+          </p>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam.
+          </p>
+        </div>
+        <div className="column">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam.
+          </p>
+        </div>
+        <div className="column">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod
+            velit sed velit aliquam, vel aliquet velit aliquam.
+          </p>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit
+        sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit sed
+        velit aliquam, vel aliquet velit aliquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        velit sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit
+        sed velit aliquam, vel aliquet velit aliquam. Sed euismod velit sed
+        velit aliquam, vel aliquet velit aliquam.
+      </p>
+    </section>
   );
 }
