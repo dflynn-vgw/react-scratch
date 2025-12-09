8e9e1cd38e450385e0447c7614cb16f13fd7edb7
David Flynn <david.flynn@vgw.co>
2025-12-09 15:53:39 +0800

    header: indicate current page

--- src/common/header.tsx
+++ src/common/header.tsx
@@ -1,17 +1,23 @@
 import './header.css';
+import { useLocation } from 'react-router';
 
 export default function Header(props: {
   links?: Array<{ text: string; url: string }>;
 }) {
+  const location = useLocation();
+
   return (
     <header>
       <nav>
         <ul>
-          {props.links?.map((link, index) => (
-            <li key={index}>
-              <a href={link.url}>{link.text}</a>
-            </li>
-          ))}
+          {props.links?.map((link, index) => {
+            const isActive = location.pathname === link.url;
+            return (
+              <li key={index} className={isActive ? 'active' : ''}>
+                <a href={link.url}>{link.text}</a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
