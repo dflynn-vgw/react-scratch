133d6066b010597737fb8cc956ddeaba78807202
David Flynn <david.flynn@vgw.co>
2025-12-10 13:20:01 +0800

    chore: remove lowercase tsx files on my remote

--- src/common/logo.tsx
+++
@@ -1,8 +0,0 @@
-export default function Logo(props: { version?: string }) {
-  const { version } = props;
-  return (
-    <span style={{ filter: 'grayscale(100%)' }} title={version}>
-      💩
-    </span>
-  );
-}
